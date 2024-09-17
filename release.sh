66a1d910fc2d62f024c805fdfecad96e75e10dbb
Har01d <power0721@gmail.com>
2024-09-17 13:25:08 +0800

    fix sceipt

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PACKAGE=$(ls -t ~/Downloads/Telegram\ Desktop/pg.*.zip | tail -1)
+PACKAGE=$(ls -t ~/Downloads/Telegram\ Desktop/pg.*.zip | head -1)
 cp "$PACKAGE" pg.zip
 ls "$PACKAGE" | awk -F/ '{print $NF}' | tee >version.txt
