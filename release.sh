d3db0233a8e5072385ded4caa346608185fe4650
Har01d <power0721@gmail.com>
2024-09-17 15:24:05 +0800

    update 20240917-1507

--- release.sh
+++ release.sh
@@ -4,3 +4,6 @@ PACKAGE=$(ls -t ~/Downloads/Telegram\ Desktop/pg.*.zip | head -1)
 cp "$PACKAGE" pg.zip
 ls "$PACKAGE" | awk -F/ '{print $NF}' | awk -F. '{print $2}' | tee >version.txt
 cat version.txt
+
+git commit -am "update $(cat version.txt)"
+git push
