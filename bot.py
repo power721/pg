80d1cfefdb8e918cece1ece099fdbcf1e1f7ab8e
Har01d <power0721@gmail.com>
2024-09-18 20:27:21 +0800

    update 20240918-2024

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 from telegram.ext import Application, MessageHandler, filters
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-MAX_FILE_SIZE = 50 * 1024 * 1024
+MAX_FILE_SIZE = 20 * 1024 * 1024
+DOWNLOAD_FOLDER = 'downloads'
 
 async def downloader(update, context):
     message = update.message
@@ -14,15 +15,15 @@ async def downloader(update, context):
         print(f"Received file: {file_name} with file ID: {file_id} and size: {file_size} bytes")
         # Check if file exceeds the size limit
         if file_size > MAX_FILE_SIZE:
-            await update.message.reply_text("Sorry, the file is too large to download (max 50MB).")
+            await update.message.reply_text("Sorry, the file is too large to download (max 20MB).")
             print(f"File {file_name} exceeds size limit. Skipping download.")
         else:
             # Download the file if within size limits
             file = await context.bot.get_file(message.document)
-            await file.download_to_drive(os.path.join("./lib", file_name))
+            await file.download_to_drive(os.path.join(DOWNLOAD_FOLDER, file_name))
 
 def main() -> None:
-    os.makedirs("./lib", exist_ok=True)
+    os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(MessageHandler(filters.Document.ALL, downloader))
--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ echo "old version: ${old_version}  new version: ${new_version}"
 
 if [ "${old_version}" != "${new_version}" ]; then
   echo "${new_version}" >version.txt
-  cat "commit"
+  echo "commit"
 
   git commit -am "update $(cat version.txt)"
   git push
