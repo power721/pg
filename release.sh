d8c229867980a2a6ade4d6db37c06ea15c09f934
Har01d <power0721@gmail.com>
2024-09-18 20:29:38 +0800

    update

--- release.sh
+++ release.sh
@@ -2,13 +2,13 @@
 
 old_version=$(cat version.txt)
 PACKAGE=$(ls -t ~/Downloads/Telegram\ Desktop/pg.*.zip | head -1)
-cp "$PACKAGE" pg.zip
+mv "$PACKAGE" pg.zip
 new_version=$(ls "$PACKAGE" | awk -F/ '{print $NF}' | awk -F. '{print $2}')
 echo "old version: ${old_version}  new version: ${new_version}"
 
 if [ "${old_version}" != "${new_version}" ]; then
   echo "${new_version}" >version.txt
-  echo "commit"
+  echo "commit file"
 
   git commit -am "update $(cat version.txt)"
   git push
